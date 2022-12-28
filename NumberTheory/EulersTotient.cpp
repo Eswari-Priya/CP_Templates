55f6ab7f6eaa4cf17d20b2e53a060f8d43bb3920
Eswari-Priya <repakaeswaripriya@example.com>
2022-12-28 18:31:13 +0530

    change in totient

--- NumberTheory/EulersTotient.cpp
+++ NumberTheory/EulersTotient.cpp
@@ -12,7 +12,6 @@ map<int,int> primeFactorization(int n){
     return mp;
 }
 
-
 double NumberOfCoPrimes(int n){
     map<int,int> factors = primeFactorization(n);
     double ans = n;
@@ -23,9 +22,28 @@ double NumberOfCoPrimes(int n){
     return ans;
 }
 
+vector<double> EulerTotient(int n){
+    vector<double> coprimes(n+1,0);
+    for(int i=0; i < n+1; i++)coprimes[i]=i;
+    for(int i=2; i < n+1; i++){
+        if(coprimes[i]==i){
+            coprimes[i]--;
+            long long x = i+i,y=i;
+            while(x <n+1){
+                coprimes[x] *= ((double)1 - (double)1/y);
+                x+=y;
+            }
+        }
+    }
+    return coprimes;
+}
+
 int main(){
     int n;
     cin>>n;
-    cout<<NumberOfCoPrimes(n);
+    vector<double> ans = EulerTotient(n);
+    for(int i=2; i < n+1; i++){
+        cout<<i<<" "<<ans[i]<<endl;
+    }
     return 0;
 }
